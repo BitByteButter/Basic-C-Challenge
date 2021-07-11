c599c0f130e0d61ca53ce4475e20fe050fdfdf7b
BitByteButter <romeokarki24@gmail.com>
2021-07-11 16:35:34 +0545

    OkFella

---
+++ includeme.h
@@ -0,0 +1,11 @@
+#include <stdio.h>  
+#include <conio.h>
+int main(int argc, char const *argv[])
+{
+    int a,b,c;
+   a = atoi(argv[1]);
+   b = atoi(argv[2]);
+   c = atoi(argv[3]);
+   printf("%d",a+b+c);  
+    
+}
